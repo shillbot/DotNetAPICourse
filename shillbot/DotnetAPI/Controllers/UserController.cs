cd2567fcb0adcac43527abf3fd9987986839f702
Shailar Brown <shillbot@shillbot.com>
2025-02-23 14:23:07 -0600

    commit before chapter 69

---
+++ shillbot/DotnetAPI/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using DotnetAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPI.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    DataContextDapper _dapper;
+
+    public UserController(IConfiguration config)
+    {
+        _dapper = new DataContextDapper(config);
+    }
+
+    [HttpGet("TestConnection")]
+    public DateTime TestConnection()
+    {
+        return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+    }
+
+    [HttpGet("GetUsers/{testValue}")]
+    public string[] GetUsers(string testValue)
+    {
+        string[] responseArray =
+		[
+			"test1", 
+			testValue
+        ];
+        return responseArray;
+    }
+}
+
