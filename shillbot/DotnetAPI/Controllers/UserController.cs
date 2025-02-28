b309b6b054b1c72fe88a23d0ca88ba27d6a59cc8
shillbot.shillbot <shillbot@shillbot.com>
2025-02-28 14:49:16 -0600

    Commit before section 8

--- shillbot/DotnetAPI/Controllers/UserController.cs
+++ shillbot/DotnetAPI/Controllers/UserController.cs
@@ -10,12 +10,12 @@ namespace DotnetAPI.Controllers;
 [ApiController]
 public class UserController(IConfiguration config) : ControllerBase
 {
-    private readonly DataContextDapper _dapper = new(config);
+	private readonly DataContextDapper _dapper = new(config);
 
-    [HttpGet("GetUsers")]
-    public IEnumerable<User> GetUsers()
-    {
-        string sql = @"
+	[HttpGet("GetUsers")]
+	public IEnumerable<User> GetUsers()
+	{
+		string sql = @"
 			SELECT UserId,
 				   FirstName,
 				   LastName,
@@ -24,14 +24,14 @@ public class UserController(IConfiguration config) : ControllerBase
 				   Active
 			FROM TutorialAppSchema.Users";
 
-        IEnumerable<User> users = _dapper.LoadData<User>(sql);
-        return users;
-    }
+		IEnumerable<User> users = _dapper.LoadData<User>(sql);
+		return users;
+	}
 
-    [HttpGet("GetUser/{userId}")]
-    public User GetUser(int userId)
-    {
-        string sql = @"
+	[HttpGet("GetUser/{userId}")]
+	public User GetUser(int userId)
+	{
+		string sql = $@"
 			SELECT UserId,
 				   FirstName,
 				   LastName,
@@ -39,30 +39,28 @@ public class UserController(IConfiguration config) : ControllerBase
 				   Gender,
 				   Active
 			FROM TutorialAppSchema.Users
-            WHERE UserId = " + userId.ToString(); // Not parameterized -- This is horrible.
-        User user = _dapper.LoadDataSingle<User>(sql);
-        return user;
-    }
+            WHERE UserId = {userId}"; // Not parameterized -- This is horrible.
+		User user = _dapper.LoadDataSingle<User>(sql);
+		return user;
+	}
 
-    [HttpPut("EditUser")]
-    public IActionResult EditUser(User user)
-    {
-        string sql = @"
+	[HttpPut("EditUser")]
+	public IActionResult EditUser(User user)
+	{
+		string sql = $@"
             UPDATE TutorialAppSchema.Users
-            SET FirstName = '" + user.FirstName + "',  LastName = '" + user.LastName + "', Email = '" + user.Email +
-                     "', Gender = '" + user.Gender + "', Active = '" + user.Active +
-                     "' WHERE UserId = " + user.UserId;
+            SET FirstName = '{user.FirstName}',  LastName = '{user.LastName}', Email = '{user.Email}', Gender = '{user.Gender}', Active = '{user.Active}' WHERE UserId = {user.UserId}";
 
-        if (_dapper.ExecuteSql(sql))
-            return Ok();
+		if (_dapper.ExecuteSql(sql))
+			return Ok();
 
-        throw new DataException("Update Failed.");
-    }
+		throw new DataException("Update Failed.");
+	}
 
-    [HttpPost("AddUser")]
-    public IActionResult AddUser(UserToAddDto userToAdd)
-    {
-        string sql = @"
+	[HttpPost("AddUser")]
+	public IActionResult AddUser(UserToAddDto userToAdd)
+	{
+		string sql = $@"
         INSERT INTO TutorialAppSchema.Users
         (
             FirstName,
@@ -73,24 +71,148 @@ public class UserController(IConfiguration config) : ControllerBase
         )
         VALUES
         (
-            '" + userToAdd.FirstName + "', '" + userToAdd.LastName + "', '" + userToAdd.Email + "', '" + userToAdd.Gender + "', '" +
-                     userToAdd.Active +
-                     "'" + ")";
-        
-        if (_dapper.ExecuteSql(sql))
-            return Ok();
-        throw new DataException("Insert Failed.");
-    }
-    
-    [HttpDelete("DeleteUser/{userId}")]
-    public IActionResult DeleteUser(int userId)
-    {
-        string sql = @"
+            '{userToAdd.FirstName}', '{userToAdd.LastName}', '{userToAdd.Email}', '{userToAdd.Gender}', '{userToAdd.Active}')";
+
+		if (_dapper.ExecuteSql(sql))
+			return Ok();
+		throw new DataException("Insert Failed.");
+	}
+
+	[HttpDelete("DeleteUser/{userId}")]
+	public IActionResult DeleteUser(int userId)
+	{
+		string sql = $@"
         DELETE FROM TutorialAppSchema.Users
-        WHERE UserId = '" + userId.ToString() + "'";
-        
-        if (_dapper.ExecuteSql(sql))
-            return Ok();
-        throw new DataException("Delete Failed.");
-    }
+        WHERE UserId = '{userId}'";
+
+		if (_dapper.ExecuteSql(sql))
+			return Ok();
+		throw new DataException("Delete Failed.");
+	}
+
+	[HttpGet("UserSalary/{userId}")]
+	public IEnumerable<UserSalary> GetUserSalary(int userId)
+	{
+		return _dapper.LoadData<UserSalary>($@"
+            SELECT UserSalary.UserId
+                    , UserSalary.Salary
+            FROM  TutorialAppSchema.UserSalary
+                WHERE UserId = {userId}");
+	}
+
+	[HttpPost("UserSalary")]
+	public IActionResult PostUserSalary(UserSalary userSalaryForInsert)
+	{
+		string sql = $@"
+            INSERT INTO TutorialAppSchema.UserSalary (
+                UserId,
+                Salary
+            ) VALUES ({userSalaryForInsert.UserId}, {userSalaryForInsert.Salary})";
+
+		if (_dapper.ExecuteSqlWithRowCount(sql) > 0)
+		{
+			return Ok(userSalaryForInsert);
+		}
+
+		throw new Exception("Adding User Salary failed on save");
+	}
+
+	[HttpPut("UserSalary")]
+	public IActionResult PutUserSalary(UserSalary userSalaryForUpdate)
+	{
+		string sql =
+			$"UPDATE TutorialAppSchema.UserSalary SET Salary={userSalaryForUpdate.Salary} WHERE UserId={userSalaryForUpdate.UserId}";
+
+		if (_dapper.ExecuteSql(sql))
+		{
+			return Ok(userSalaryForUpdate);
+		}
+
+		throw new Exception("Updating User Salary failed on save");
+	}
+
+	[HttpDelete("UserSalary/{userId}")]
+	public IActionResult DeleteUserSalary(int userId)
+	{
+		string sql = $"DELETE FROM TutorialAppSchema.UserSalary WHERE UserId={userId}";
+
+		if (_dapper.ExecuteSql(sql))
+		{
+			return Ok();
+		}
+
+		throw new Exception("Deleting User Salary failed on save");
+	}
+
+	[HttpGet("UserJobInfo/{userId}")]
+	public IEnumerable<UserJobInfo> GetUserJobInfo(int userId)
+	{
+		return _dapper.LoadData<UserJobInfo>($@"
+            SELECT  UserJobInfo.UserId
+                    , UserJobInfo.JobTitle
+                    , UserJobInfo.Department
+            FROM  TutorialAppSchema.UserJobInfo
+                WHERE UserId = {userId}");
+	}
+
+	[HttpPost("UserJobInfo")]
+	public IActionResult PostUserJobInfo(UserJobInfo userJobInfoForInsert)
+	{
+		string sql = $@"
+            INSERT INTO TutorialAppSchema.UserJobInfo (
+                UserId,
+                Department,
+                JobTitle
+            ) VALUES ({userJobInfoForInsert.UserId}, '{userJobInfoForInsert.Department}', '{userJobInfoForInsert.JobTitle}')";
+
+		if (_dapper.ExecuteSql(sql))
+		{
+			return Ok(userJobInfoForInsert);
+		}
+
+		throw new Exception("Adding User Job Info failed on save");
+	}
+
+	[HttpPut("UserJobInfo")]
+	public IActionResult PutUserJobInfo(UserJobInfo userJobInfoForUpdate)
+	{
+		string sql =
+			$"UPDATE TutorialAppSchema.UserJobInfo SET Department='{userJobInfoForUpdate.Department}', JobTitle='{userJobInfoForUpdate.JobTitle}' WHERE UserId={userJobInfoForUpdate.UserId}";
+
+		if (_dapper.ExecuteSql(sql))
+		{
+			return Ok(userJobInfoForUpdate);
+		}
+
+		throw new Exception("Updating User Job Info failed on save");
+	}
+
+	// [HttpDelete("UserJobInfo/{userId}")]
+	// public IActionResult DeleteUserJobInfo(int userId)
+	// {
+	//     string sql = "DELETE FROM TutorialAppSchema.UserJobInfo  WHERE UserId=" + userId;
+
+	//     if (_dapper.ExecuteSql(sql))
+	//     {
+	//         return Ok();
+	//     }
+	//     throw new Exception("Deleting User Job Info failed on save");
+	// }
+
+	[HttpDelete("UserJobInfo/{userId}")]
+	public IActionResult DeleteUserJobInfo(int userId)
+	{
+		string sql = $@"
+            DELETE FROM TutorialAppSchema.UserJobInfo 
+                WHERE UserId = '{userId}'";
+
+		Console.WriteLine(sql);
+
+		if (_dapper.ExecuteSql(sql))
+		{
+			return Ok();
+		}
+
+		throw new Exception("Failed to Delete User");
+	}
 }
--- shillbot/DotnetAPI/Controllers/UserEfController.cs
+++ shillbot/DotnetAPI/Controllers/UserEfController.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using AutoMapper;
 using DotnetAPI.Data;
 using DotnetAPI.Dtos;
 using DotnetAPI.Models;
@@ -11,6 +12,7 @@ namespace DotnetAPI.Controllers;
 public class UserEfController(IConfiguration config) : ControllerBase
 {
 	private readonly DataContextEf _context = new(config);
+	private IMapper _mapper = new Mapper(new MapperConfiguration(cfg => { cfg.CreateMap<UserToAddDto, User>(); }));
 
 	[HttpGet("GetUsers")]
 	public IEnumerable<User> GetUsers()
@@ -18,7 +20,7 @@ public class UserEfController(IConfiguration config) : ControllerBase
 		IEnumerable<User> users = _context.User.ToList();
 		return users;
 	}
-	
+
 	[HttpGet("GetUser/{userId}")]
 	public User GetUser(int userId)
 	{
@@ -42,34 +44,148 @@ public class UserEfController(IConfiguration config) : ControllerBase
 			if (_context.SaveChanges() > 0)
 				return Ok();
 		}
+
 		throw new DataException("EditUser Failed.");
 	}
 
 	[HttpPost("AddUser")]
 	public IActionResult AddUser(UserToAddDto userToAdd)
 	{
-		User userEf = new()
-					  {
-						  FirstName = userToAdd.FirstName,
-						  LastName = userToAdd.LastName,
-						  Email = userToAdd.Email,
-						  Gender = userToAdd.Gender,
-						  Active = userToAdd.Active
-					  };
+		User userEf = _mapper.Map<User>(userToAdd);
 		_context.User.Add(userEf);
-		if(_context.SaveChanges() > 0)
+		if (_context.SaveChanges() > 0)
 			return Ok();
 		throw new DataException("AddUser Failed.");
 	}
-	
+
 	[HttpDelete("DeleteUser/{userId}")]
 	public IActionResult DeleteUser(int userId)
 	{
 		User? user = _context.User.Find(userId);
 		if (user != null)
 			_context.User.Remove(user);
-		if(_context.SaveChanges() > 0)
+		if (_context.SaveChanges() > 0)
 			return Ok();
 		throw new DataException("DeleteUser Failed.");
+	}
+
+	[HttpGet("UserSalary/{userId}")]
+	public IEnumerable<UserSalary> GetUserSalaryEF(int userId)
+	{
+		return _context.UserSalary
+			.Where(u => u.UserId == userId)
+			.ToList();
+	}
+
+	[HttpPost("UserSalary")]
+	public IActionResult PostUserSalaryEf(UserSalary userForInsert)
+	{
+		_context.UserSalary.Add(userForInsert);
+		if (_context.SaveChanges() > 0)
+		{
+			return Ok();
+		}
+
+		throw new Exception("Adding UserSalary failed on save");
+	}
+
+
+	[HttpPut("UserSalary")]
+	public IActionResult PutUserSalaryEf(UserSalary userForUpdate)
+	{
+		UserSalary? userToUpdate = _context.UserSalary
+			.FirstOrDefault(u => u.UserId == userForUpdate.UserId);
+
+		if (userToUpdate != null)
+		{
+			_mapper.Map(userForUpdate, userToUpdate);
+			if (_context.SaveChanges() > 0)
+			{
+				return Ok();
+			}
+
+			throw new Exception("Updating UserSalary failed on save");
+		}
+
+		throw new Exception("Failed to find UserSalary to Update");
+	}
+
+
+	[HttpDelete("UserSalary/{userId}")]
+	public IActionResult DeleteUserSalaryEf(int userId)
+	{
+		UserSalary? userToDelete = _context.UserSalary
+			.FirstOrDefault(u => u.UserId == userId);
+
+		if (userToDelete != null)
+		{
+			_context.UserSalary.Remove(userToDelete);
+			if (_context.SaveChanges() > 0)
+			{
+				return Ok();
+			}
+
+			throw new Exception("Deleting UserSalary failed on save");
+		}
+
+		throw new Exception("Failed to find UserSalary to delete");
+	}
+
+
+	[HttpGet("UserJobInfo/{userId}")]
+	public IEnumerable<UserJobInfo> GetUserJobInfoEF(int userId)
+	{
+		return _context.UserJobInfo
+			.Where(u => u.UserId == userId)
+			.ToList();
+	}
+
+	[HttpPost("UserJobInfo")]
+	public IActionResult PostUserJobInfoEf(UserJobInfo userForInsert)
+	{
+		_context.UserJobInfo.Add(userForInsert);
+		if (_context.SaveChanges() > 0)
+		{
+			return Ok();
+		}
+		throw new Exception("Adding UserJobInfo failed on save");
+	}
+
+
+	[HttpPut("UserJobInfo")]
+	public IActionResult PutUserJobInfoEf(UserJobInfo userForUpdate)
+	{
+		UserJobInfo? userToUpdate = _context.UserJobInfo
+			.FirstOrDefault(u => u.UserId == userForUpdate.UserId);
+
+		if (userToUpdate != null)
+		{
+			_mapper.Map(userForUpdate, userToUpdate);
+			if (_context.SaveChanges() > 0)
+			{
+				return Ok();
+			}
+			throw new Exception("Updating UserJobInfo failed on save");
+		}
+		throw new Exception("Failed to find UserJobInfo to Update");
+	}
+
+
+	[HttpDelete("UserJobInfo/{userId}")]
+	public IActionResult DeleteUserJobInfoEf(int userId)
+	{
+		UserJobInfo? userToDelete = _context.UserJobInfo
+			.FirstOrDefault(u => u.UserId == userId);
+
+		if (userToDelete != null)
+		{
+			_context.UserJobInfo.Remove(userToDelete);
+			if (_context.SaveChanges() > 0)
+			{
+				return Ok();
+			}
+			throw new Exception("Deleting UserJobInfo failed on save");
+		}
+		throw new Exception("Failed to find UserJobInfo to delete");
 	}
 }
